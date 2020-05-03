03f19294bd3513ac4dc1ecfe81891fea9efeb887
blanks009 <41790800+blanks009@users.noreply.github.com>
2020-05-03 22:11:08 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,42 @@
+//Server initialisieren
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var socketIO = require('socket.io');
+
+//prepare Server
+var app = express();
+var server = http.Server(app);
+var io = socketIO(server);
+
+app.set('port', 5000);
+app.use('/static', express.static(__dirname + '/static'));
+
+// Routing
+//Ein Spiel starten
+app.get('/', function(request, response) {
+    response.sendFile(path.join(__dirname, 'index.html'));
+});
+//Einem Spiel beitreten
+app.get('/room', function(request, response) {
+    response.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Starts the server.
+server.listen(5000, function() {
+    console.log('Starting server on port 5000');
+});
+
+
+// Add the WebSocket handlers
+io.on('connection', function(socket) {
+    socket.on('hello', function(data) {
+        console.log(data);      
+    });
+});
+
+setInterval(function() {
+    io.sockets.emit('message', 'hi!');
+  }, 1000);
+
+
